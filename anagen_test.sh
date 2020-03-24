b6f545a3f894a630edc3a3773050f533a40c4811
Hanson Lu <hansonlu@uchicago.edu>
2020-03-24 13:29:56 -0500

    move things to anagen repo

--- anagen_test.sh
+++
@@ -1,4 +0,0 @@
-GPU=0 python anagen_test_eval.py bert_base \
-    --from_npy outputs/bert_base.eval_out.npy \
-    --use_l1 \
-    --anagen_model_dir /home/hansonlu/links/data/anagen_models/anagen_anaph_only_b28_lr_default
--- anagen_test_train.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-PP_DIR=/home/hansonlu/links/data/pp_coref_anagen
-python anagen_test_train.py \
-    --train_jsonlines $PP_DIR/train.english.256.anagen.jsonlines \
-    --eval_jsonlines $PP_DIR/dev.english.256.anagen.jsonlines \
-    --gpu \
-    --train_epochs 3
