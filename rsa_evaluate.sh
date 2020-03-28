382eeffbd2096d6837c0fe9deb93998b23519518
Hanson Lu <hansonlu@uchicago.edu>
2020-03-28 15:29:25 -0500

    add rsa_evaluate.py

--- rsa_evaluate.sh
+++ rsa_evaluate.sh
@@ -1,5 +1,4 @@
-# GPU=0 python evaluate.py bert_base \
-#     --from_npy outputs/bert_base.eval_out.npy \
+# GPU=0 python rsa_evaluate.py outputs/bert_base.eval_out.npy \
 #     --use_l1 \
 #     --batch_size 64 \
 #     --max_num_ctxs_in_batch 1 \
@@ -7,5 +6,4 @@
 #     --s0_model_path /home/hansonlu/links/data/rnn_anagen_models/rnn_anagen_0325_basic_batch_768_best.ckpt \
 
 # --anagen_model_dir /home/hansonlu/links/data/anagen_models/anagen_anaph_only_b28_lr_default
-GPU=0 python evaluate.py bert_base \
-    --from_npy outputs/bert_base.eval_out.npy \
+GPU=0 python rsa_evaluate.py outputs/bert_base.eval_out.npy \
